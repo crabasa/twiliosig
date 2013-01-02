ff93e5015edfb88e3f08255abe535c03d91b4c81
Kevin Whinnery <kevin.whinnery@gmail.com>
2013-01-02 15:01:36 -0600

    whoops, meant to add colon and dashes as well.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var crypto = require('crypto');
 
 // Utility Functions
 exports.valid = function(request, authKey) {
-   var url = request.protocol + request.headers.host + request.url;
+   var url = request.protocol + '://' + request.headers.host + request.url;
    Object.keys(request.body).sort().forEach(function(key, i) {
        url = url + key + request.body[key];
    });
